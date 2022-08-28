64c8269354af84425aad74db7e9e6091cc03ee88
ryuya0416 <67462677+ryuya416@users.noreply.github.com>
2022-08-28 09:52:50 +0900

    ✨ create data_display

---
+++ src/pages/data_display.tsx
@@ -0,0 +1,66 @@
+import {
+  Avatar,
+  BackgroundImage,
+  Badge,
+  Box,
+  Button,
+  Card,
+  Center,
+  Group,
+  Text,
+  Image,
+} from "@mantine/core";
+import { Layout } from "../components/Layout";
+
+const Data_Display = () => {
+  return (
+    <Layout title="data_display">
+      <Avatar
+        radius="xl"
+        src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=250&q=80"
+      />
+      <Text mt={24}>backgroudImage</Text>
+      <Box sx={{ maxWidth: 300 }} mx="auto">
+        <BackgroundImage
+          src="https://images.unsplash.com/photo-1419242902214-272b3f66ee7a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80"
+          radius="sm"
+        >
+          <Center p="md">
+            <Text color="#fff">
+              BackgroundImage component can be used to add any content on image.
+              It is useful for hero headers and other similar sections
+            </Text>
+          </Center>
+        </BackgroundImage>
+      </Box>
+      <Text mt={24}>Card</Text>
+      <Card shadow="sm" p="lg" radius="md" withBorder>
+        <Card.Section>
+          <Image
+            src="https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80"
+            height={160}
+            alt="Norway"
+          />
+        </Card.Section>
+
+        <Group position="apart" mt="md" mb="xs">
+          <Text weight={500}>Norway Fjord Adventures</Text>
+          <Badge color="pink" variant="light">
+            On Sale
+          </Badge>
+        </Group>
+
+        <Text size="sm" color="dimmed">
+          With Fjord Tours you can explore more of the magical fjord landscapes
+          with tours and activities on and around the fjords of Norway
+        </Text>
+
+        <Button variant="light" color="blue" fullWidth mt="md" radius="md">
+          Book classic tour now
+        </Button>
+      </Card>
+    </Layout>
+  );
+};
+
+export default Data_Display;
